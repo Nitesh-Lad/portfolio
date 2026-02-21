f73bcf7ad445637a8e9ee29eefcc0e424e231fa0
Nitesh <niteshlad07@gmail.com>
2026-02-22 00:46:21 +0530

    update

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,4 @@
-import HeroImg from "@/assets/images/hero.jpeg";
+import HeroImg from "@/assets/images/hero.jpg";
 import OlovaLogo from "@/assets/images/olova.png";
 
 export default function About() {
